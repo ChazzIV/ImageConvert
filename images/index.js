9fcdc1d8d8e236d3c5397817c9740a761a9f4256
Marcelo Alberto Villalpando Martinez <marcelovillalpandomtz@gmail.com>
2021-04-14 18:42:03 -0500

    consulta de prueba

--- images/index.js
+++ images/index.js
@@ -9,3 +9,23 @@ const server = new server_1.default();
 server.start(() => {
     console.log(`Servidor image corriendo en el puerto ${server.port}`);
 });
+//conectando a la base de datos
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'purchasing'
+});
+connection.connect((err) => {
+    if (err)
+        throw err;
+    console.log('conectado a mysql');
+});
+//consulta de prueba
+connection.query('select  concat(file_url, file_name) as image from product_images; ', (err, rows) => {
+    if (err)
+        throw err;
+    console.log('data received from db');
+    console.log(rows);
+});
--- index.ts
+++ index.ts
@@ -7,3 +7,29 @@ const server = new Server();
 server.start( () => {
     console.log(`Servidor image corriendo en el puerto ${server.port}`);
 });
+
+
+//conectando a la base de datos
+
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'purchasing'
+});
+
+connection.connect((err: any) => {
+    if (err) throw err;
+    console.log('conectado a mysql');
+});
+
+
+//consulta de prueba
+
+connection.query('select  concat(file_url, file_name) as image from product_images; ', (err: any, rows: any) => {
+    if(err) throw err;
+
+    console.log('data received from db');
+    console.log(rows);
+});
